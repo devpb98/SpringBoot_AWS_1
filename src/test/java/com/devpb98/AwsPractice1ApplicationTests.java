8ca821be198e893a56891e06c597282e1eaa26c7
devpb98 <devpb1998@gmail.com>
2025-12-26 01:52:17 +0530

    Initial commit

---
+++ src/test/java/com/devpb98/AwsPractice1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devpb98;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsPractice1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
