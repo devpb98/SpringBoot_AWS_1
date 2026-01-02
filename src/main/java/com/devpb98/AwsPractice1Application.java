7e841e8cafed4bd9cda046b0430b82afbf02b838
Dev Prasad Bashyal <devpb1998@gmail.com>
2026-01-02 17:09:16 +0530

    Github Test

--- src/main/java/com/devpb98/AwsPractice1Application.java
+++ src/main/java/com/devpb98/AwsPractice1Application.java
@@ -8,6 +8,8 @@ public class AwsPractice1Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AwsPractice1Application.class, args);
+
+		System.out.println("hello");
 	}
 
 }
